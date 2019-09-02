0f4f03ec01d99d9302691f11aed185a6a643cbbf
kel-github <getkellygarner@gmail.com>
2019-09-02 10:20:27 +1000

    fixed update bug

--- EWA/EWA.R
+++ EWA/EWA.R
@@ -13,7 +13,8 @@ ewa_update <- function(experience_decay_factor, payoff_decay_factor, last_experi
   # for the previous choice
   experience_weight = last_experience_weight * experience_decay_factor + 1
   choice_value = (last_choice_value * payoff_decay_factor * last_experience_weight + previous_outcome) / experience_weight
-  choice_value
+  out = list( choice_value = choice_value, experience_weight = experience_weight )
+  out
 }
 
 action_selection <- function( choice_value, other_choice_value, inverse_temperature ){
@@ -60,40 +61,51 @@ experience_weights = c()
 outcomes = c() # empty vector for outcomes
 ntrials = 50 # will do 50 before switching
 
+start_choice_values = .1 # to update from, the first time a stimulus is chosen or experienced
+start_exp_weights = .1 
 ###############################################################################################################################
 # run simulation
 ###############################################################################################################################
 for (i in 1:ntrials){
   
+  # get the choice made on trial i
   if (i == 1){
     # assuming first choice is 50:50, 0 is blue, 1 is orange
-    choice[i] = sample(choices, 1, prob = c(.5, .5) )
-    choice_values[i] = .5 # because it is 50/50 at this point
-    experience_weights[i] = .5
+    choice[i] = sample(choices, 1, prob = c(.5, .5) ) # choice on the 1st trial is a .5/.5 shot
+  } else {
+    choice[i] = sample(choices, 1, prob = next_trials_action_probs)
   }
-  else {
-    
-    # get choice values and experience weights for updates
-    tmp_choice_values = choice_values[c(max(which(choice == "blue")), max(which(choice == "orange")))] # this gets the last value for each choice (don't worry about the warning msg)
-    tmp_choice_values[is.na(tmp_choice_values)] = .5 # just in case one has not assigned a value yet
-    tmp_experience_weights = experience_weights[ c(max(which(choice == "blue")), max(which(choice == "orange")))  ]
-    tmp_experience_weights[is.na(tmp_experience_weights)] = .5
-    
-    # convert each of these into the new weight value (i.e. apply update rule to last chosen, but pass through next value)
-    choice_value_for_update = tmp_choice_values[ which(choices == choice[i-1])   ]
-    experience_weight_for_update = tmp_experience_weights[ which( choices == choice[i-1]) ]
-    new_choice_value = ewa_update( experience_decay_factor, payoff_decay_factor, experience_weight_for_update, choice_value_for_update, outcomes[i-1] )
-    # assign to the choice values to be compared for action generation
-    tmp_choice_values[ which(choices == choice[i-1])   ] = new_choice_value
-    # feed in to get action probabilities
-    this_trials_action_probs = c( action_selection( tmp_choice_values[1], tmp_choice_values[2], inverse_temperature ), action_selection(  tmp_choice_values[2], tmp_choice_values[1], inverse_temperature   ) )
-    choice[i] = sample(choices, 1, prob = this_trials_action_probs)
+  # get the outcome for the choice that was just made
+  outcomes[i] = get_trial_outcome(choice[i], outcome_probs) # get the outcome for the current trial
+  
+  # now update the choice value based on the outcome
+  # first, get the values for last time you encountered each stimulus
+  if( i == 1 ) { # if neither stimulus has been chosen before, get the starting values for update
+    tmp_choice_values = c( start_choice_values, start_choice_values )
+    tmp_experience_weights = c( start_exp_weights, start_exp_weights )
+  } else {
+    tmp_choice_values = choice_values[c(max(which(choice[1:(i-1)] == "blue")), max(which(choice[1:(i-1)] == "orange")))] 
+    tmp_choice_values[is.na(tmp_choice_values)] = start_choice_values # just in case one has not assigned a value yet
+    tmp_experience_weights = experience_weights[ c(max(which(choice[1:(i-1)] == "blue")), max(which(choice[1:(i-1)] == "orange")))  ] # get the experience weights from your last encounter with each choice stimulus
+    tmp_experience_weights[is.na(tmp_experience_weights)] = start_exp_weights
   }
-  
-  # get trial outcome
-  outcomes[i] = get_trial_outcome(choice[i], outcome_probs)
-  
+ 
+  # now select and update the choice value and experience weights for the stimulus-outcome pairing you just experienced
+  choice_value_for_update = tmp_choice_values[ which(choices == choice[i])   ]
+  experience_weight_for_update = tmp_experience_weights[ which( choices == choice[i]) ] 
+  # now we update the choice values and the experience weights for the last thing you did
+  new_choice_list = ewa_update( experience_decay_factor, payoff_decay_factor, experience_weight_for_update, choice_value_for_update, outcomes[i] )
+  # these are the values that reflect your most up to date experience which you should keep for next time
+  choice_values[i] = new_choice_list$choice_value # update the thing you chose last time with the new choice value
+  experience_weights[i] = new_choice_list$experience_weight # 
+      
+  # assign to the choice values to be compared for next action generation
+  tmp_choice_values[ which(choices == choice[i])   ] = choice_values[i] 
+  # feed in to get action probabilities
+  next_trials_action_probs = c( action_selection( tmp_choice_values[1], tmp_choice_values[2], inverse_temperature ), action_selection(  tmp_choice_values[2], tmp_choice_values[1], inverse_temperature   ) )
+    
 }
+  
 
 ###############################################################################################################################
 # make into a dataframe and save to wideform for further use
